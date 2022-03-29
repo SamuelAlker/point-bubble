Gradient Tape fixed, in a trainable state

models.py
@@ -1,4 +1,6 @@
-from tensorflow.keras import layers, Model, losses, metrics, regularizers, activations
+from tensorflow.keras import layers, Model, losses, metrics, regularizers, activations, backend
+from keras.engine import data_adapter
+import tensorflow as tf
 
 
 def identity_block(x, filter, activation):
@@ -32,12 +34,13 @@ def convolutional_block(x, filter, activation):
     # Add Residue
     x = layers.Add()([x, x_skip])
     x = layers.Activation(activation=activation)(x)
+
     return x
 
 
 def resnet(activation, optimizer, input_shape, frames):
     # Step 1 (Setup Input Layer)
-    x_input = layers.Input(shape=(frames, input_shape, 2), name="input")
+    x_input = layers.Input(shape=(frames, input_shape, 4), name="input")
     x = layers.ZeroPadding2D((0, 3))(x_input)
     # Step 2 (Initial Conv layer along with maxPool)
     x = layers.Conv2D(64, (frames, 7), strides=(1, 2), activation='tanh', padding='same')(x)
@@ -69,7 +72,7 @@ def resnet(activation, optimizer, input_shape, frames):
     x = layers.Dense(input_shape*2, activation=activations.linear, kernel_regularizer=regularizers.l2(0.001))(x)
     x = layers.Reshape((input_shape, 2))(x)
     model = Model(x_input, x)
-    print(model.summary())
+    # print(model.summary())
 
     stringlist = []
     model.summary(print_fn=lambda x: stringlist.append(x))
@@ -81,3 +84,146 @@ def resnet(activation, optimizer, input_shape, frames):
     metric = metrics.MeanAbsoluteError(name="MAE")
     model.compile(optimizer=optimizer, loss=losses.MeanSquaredError(), metrics=[metric], )
     return [model, sum]
+
+
+def residual_cell(x, activation, layer_size=2, size=10):
+    x_skip = x
+    for i in range(layer_size):
+        x = layers.Dense(size, activation=activation)(x)
+    input_size = x_skip.get_shape()[1]
+    x = layers.Dense(input_size, activation=activation)(x)
+    x = layers.Add()([x, x_skip])
+    x = layers.Activation(activation=activation)(x)
+    return x
+
+
+def message_model(frames, input_number, output_number, activation, residual_cells=None):
+    if residual_cells is None:
+        residual_cells = [[2, 5], [2, 5]]
+    x_input = layers.Input(shape=(frames, input_number), name="message_input")
+    x = layers.Flatten()(x_input)
+    for cell_struct in residual_cells:
+        x = residual_cell(x, activation, layer_size=cell_struct[0], size=cell_struct[1])
+    x = layers.Dense(output_number, activation=activation)(x)
+    model = Model(x_input, x)
+    return model
+
+
+def interpretation_model(input_number, output_number, activation, residual_cells=None):
+    if residual_cells is None:
+        residual_cells = [[2, 10], [2, 10]]
+    x_input = layers.Input(shape=input_number, name="interpretation_input")
+    x = layers.Dense(30, activation=activation)(x_input)
+    for cell_struct in residual_cells:
+        x = residual_cell(x, activation, layer_size=cell_struct[0], size=cell_struct[1])
+    x = layers.Dense(output_number, activation=activation)(x)
+    model = Model(x_input, x)
+    return model
+
+
+def graph_network(frames, activation, m_input, m_output, i_output):
+    m_model = message_model(frames, m_input, m_output, activation)
+    m_model.compile()
+    i_model = interpretation_model(m_output, i_output, activation)
+    i_model.compile()
+    model = CustomModel(m_model, i_model, m_output)
+    model.compile()
+    return model
+
+
+class CustomModel(Model):
+    mse = metrics.MeanSquaredError(name="MSE")
+
+    def loss_function(self, y_true, y_pred):
+        loss = losses.mean_squared_error(y_true, y_pred)
+        return loss
+
+    def __init__(self, m_model, i_model, m_model_output):
+        super(CustomModel, self).__init__()
+        self.m_model = m_model
+        self.m_model_output = m_model_output
+        self.i_model = i_model
+
+    def call(self, inputs, training=None, mask=None):
+        shape = inputs.shape
+        message_sum = self.m_model(inputs[:, 1])
+        for i in range(1, shape[1]):
+            print("call", i)
+            message_sum += self.m_model(inputs[:, i])
+        message_sum += inputs[:, 0, 0]
+        output = self.i_model(message_sum)
+        output = tf.expand_dims(output, axis=1)
+        for index in range(1, shape[1]):
+            print(index)
+            message_sum = self.m_model(inputs[:, 0])
+            message_sum += inputs[:, index, 0]
+            for i in range(1, shape[1]):
+                if i != index:
+                    message_sum += self.m_model(inputs[:, i])
+            output = tf.concat((output, tf.expand_dims(self.i_model(message_sum), axis=1)), axis=1)
+        return output
+
+    @tf.function
+    def train_step(self, data):
+        x, y, sample_weight = data_adapter.unpack_x_y_sample_weight(data)
+        shape = x.shape
+        loss_tot = 0.
+
+        # def grad_loss(training_data, labels, ind):
+        #     with tf.GradientTape(persistent=True, watch_accessed_variables=False) as tape:
+        #         tape.watch(self.i_model.trainable_variables)
+        #         # tape_1.watch(self.m_model.trainable_variables)
+        #         message = self.m_model(training_data[:, ind - 1], training=True)
+        #         message = tf.expand_dims(message, 0)
+        #         for i in range(0, shape[1] - 1):
+        #             if i != ind and i != ind - 1:
+        #                 message = tf.concat(
+        #                     [message, tf.expand_dims(self.m_model(training_data[:, i], training=True), 0)], 0)
+        #         message = tf.concat([message, tf.expand_dims(tf.cast(x[:, ind, 0], tf.float32), 0)], 0)
+        #         message = backend.sum(message, axis=0)
+        #         pred = self.i_model(message, training=True)
+        #         loss_ = losses.mean_squared_error(labels[:, j], pred)
+
+        with tf.GradientTape(persistent=False, watch_accessed_variables=False) as tape_1:
+            tape_1.watch(self.i_model.trainable_variables)
+            tape_1.watch(self.m_model.trainable_variables)
+            index = 0
+            for single in x:
+                messages = self.m_model(single, training=True)
+                # message_sum = tf.expand_dims(message_sum, 0)
+                # for i in range(0, shape[1]-1):
+                #     if i != index and i != index-1:
+                #         message_sum = tf.concat([message_sum, tf.expand_dims(self.m_model(x[:, i], training=True), 0)], 0)
+                col_index = 0
+                for col in single:
+                    message_sum = tf.concat([messages,  tf.expand_dims(tf.cast(col[0], tf.float32), 0)], 0)
+                    message_sum = tf.expand_dims(backend.sum(message_sum, axis=0), 0)
+                    y_pred = self.i_model(message_sum, training=True)[0]
+                    y_true = tf.cast(y[index, col_index], tf.float32)
+                    loss_tot += losses.mean_squared_error(y_true, y_pred)
+                    col_index += 1
+                index += 1
+        self.optimizer.minimize(loss_tot, [self.i_model.trainable_variables, self.m_model.trainable_variables], tape=tape_1)
+
+        # self.optimizer.apply_gradients(zip(i_model_grads, i_model_trainable))
+        # self.optimizer.minimize(loss_1, self.i_model.trainable_variables, tape=tape_1)
+        # with tf.GradientTape(persistent=True) as tape_2:
+        #     shape = x.shape
+        #     message_sum = self.m_model(x[:, 0], training=True)
+        #     for i in range(1, shape[1]):
+        #         message_sum += self.m_model(x[:, i], training=True)
+        #     y_pred = self.i_model(message_sum, training=True)
+        #     loss_2 = losses.mean_squared_error(y, y_pred)
+        # self.optimizer.minimize(loss_1, self.i_model.trainable_variables, tape=tape)
+        # self.optimizer.minimize(loss_2, self.m_model.trainable_variables, tape=tape_2)
+        # y_pred = self(x, training=False)
+        # self.mse.update_state(y, y_pred)
+        # mse_result = self.mse.result()
+        return {"MSE": loss_tot}
+
+    def test_step(self, data):
+        x, y, sample_weight = data_adapter.unpack_x_y_sample_weight(data)
+        # y_pred = self(x, training=False)
+        # self.mse.update_state(y, y_pred)
+        # mse_result = self.mse.result()
+        return {"MSE": 1}

training.py
@@ -1,7 +1,8 @@
 import numpy as np
 import glob
 from tqdm import tqdm
-from tensorflow.keras import layers, models, initializers, activations, losses, metrics, optimizers, Model
+from tensorflow.keras import layers, initializers, activations, losses, metrics, optimizers, Model
+import tensorflow as tf
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 import imageio
@@ -40,123 +41,6 @@ def inception_cell(model, activation, axis, kernal_size):
     return model
 
 
-def model_1(activation, optimizer, frames=4, size=100, kernal_size=32):
-    initializer = initializers.HeNormal()
-    model = models.Sequential()
-    model.add(layers.Conv2D(kernal_size, (frames, 3), kernel_initializer=initializer, activation=activation,
-                            input_shape=(frames, size, 2)))
-    model.add(layers.Reshape((size - 2, kernal_size)))
-    model.add(layers.Conv1D(kernal_size, 3, activation=activation, kernel_initializer=initializer))
-    model = inception_cell(model, activation, 2, 32)
-    model.add(layers.MaxPool1D(3))
-    model.add(layers.Conv1D(128, 3, activation=activation, kernel_initializer=initializer))
-    model = inception_cell(model, activation, 2, 32)
-    model.add(layers.MaxPool1D(3))
-    model.add(layers.Conv1D(128, 3, activation=activation, kernel_initializer=initializer))
-    model = inception_cell(model, activation, 2, 32)
-    model.add(layers.Conv1DTranspose(kernal_size, 3, activation=activation, kernel_initializer=initializer))
-    model.add(layers.UpSampling1D(3))
-    model.add(layers.Conv1DTranspose(kernal_size, 3, activation=activation, kernel_initializer=initializer))
-    model.add(layers.UpSampling1D(3))
-    model.add(layers.Conv1DTranspose(kernal_size, 3, activation=activation, kernel_initializer=initializer))
-    model.add(layers.Conv1DTranspose(kernal_size, 3, activation=activation, kernel_initializer=initializer))
-    model.add(layers.Conv1DTranspose(kernal_size, 3, activation=activation, kernel_initializer=initializer))
-    model.add(layers.Conv1D(2, 3, activation=activation, kernel_initializer=initializer))
-
-    stringlist = []
-    model.summary(print_fn=lambda x: stringlist.append(x))
-    print(model.summary())
-    short_model_summary = "\n".join(stringlist)
-    model.compile(optimizer=optimizer, loss=losses.MeanSquaredError(), metrics=[metrics.MeanSquaredError(name="MSE")])
-    return model
-
-
-def model_2(activation, optimizer, frames=4, size=100, kernal_size=32):
-    initializer = initializers.HeNormal()
-    model = models.Sequential()
-    model.add(layers.Conv2D(kernal_size, (frames, 3), kernel_initializer=initializer, activation=activation,
-                            input_shape=(frames, size, 2)))
-    model.add(layers.Reshape((size - 2, kernal_size)))
-    model.add(layers.Conv1D(kernal_size, 3, activation=activation, kernel_initializer=initializer))
-    model = inception_cell(model, activation, 2, 32)
-    model.add(layers.MaxPool1D(3))
-    model.add(layers.Conv1D(128, 3, activation=activation, kernel_initializer=initializer))
-    model = inception_cell(model, activation, 2, 32)
-    model.add(layers.MaxPool1D(3))
-    model.add(layers.Conv1D(128, 3, activation=activation, kernel_initializer=initializer))
-    model = inception_cell(model, activation, 2, 32)
-    model.add(layers.Flatten())
-    model.add(layers.Dense(400, activation=activation))
-    model.add(layers.Dense(200, activation=activation))
-    model.add(layers.Reshape((100, 2)))
-    stringlist = []
-    model.summary(print_fn=lambda x: stringlist.append(x))
-    print(model.summary())
-    short_model_summary = "\n".join(stringlist)
-    model.compile(optimizer=optimizer, loss=losses.MeanSquaredLogarithmicError(),
-                  metrics=[metrics.MeanSquaredError(name="MSE")])
-    return model
-
-
-def model_3(activation, optimizer, frames=4, size=100, kernal_size=32):
-    initializer = initializers.HeNormal()
-    model = models.Sequential()
-    model.add(layers.Conv2D(kernal_size, (frames, 3), kernel_initializer=initializer, activation=activation,
-                            input_shape=(frames, size, 2)))
-    model.add(layers.Reshape((size - 2, kernal_size)))
-    model.add(layers.Conv1D(kernal_size, 3, activation=activation, kernel_initializer=initializer))
-    model = inception_cell(model, activation, 2, 32)
-    model.add(layers.MaxPool1D(3))
-    model.add(layers.Conv1D(128, 3, activation=activation, kernel_initializer=initializer))
-    model = inception_cell(model, activation, 2, 32)
-    model.add(layers.MaxPool1D(3))
-    model.add(layers.Conv1D(128, 3, activation=activation, kernel_initializer=initializer))
-    model = inception_cell(model, activation, 2, 32)
-    model.add(layers.Flatten())
-    model.add(layers.Dense(20, activation=activation))
-    model.add(layers.Dense(20, activation=activation))
-    model.add(layers.Dense(200, activation=activations.linear))
-    model.add(layers.Reshape((100, 2)))
-    stringlist = []
-    model.summary(print_fn=lambda x: stringlist.append(x))
-    print(model.summary())
-    short_model_summary = "\n".join(stringlist)
-    model.compile(optimizer=optimizer, loss=losses.MeanSquaredError(), metrics=[metrics.MeanSquaredError(name="MSE")])
-    return model
-
-
-def load_data(points, frames):
-    training_data = []
-    labels = []
-    pbar = tqdm(total=11)
-    for simulation in range(11):
-        pbar.update()
-        data_names = glob.glob("training_data/Simulation_{}_points_{}/*".format(simulation, points))
-        folder_length = len(data_names)
-        data_array = []
-        for file_num in range(3, folder_length + 1):
-            data = np.load("training_data/Simulation_{}_points_{}/data_{}.npy".format(simulation, points, file_num))
-            data_array.append(data)
-
-        xy_data = []
-        for data in data_array:
-            xy_array = []
-            for i in range(points):
-                xy = []
-                for j in range(2):
-                    xy.append(data[j][i])
-                xy_array.append(xy)
-            xy_data.append(xy_array)
-        for i in range(folder_length - frames - 2):
-            single = []
-            for j in range(frames):
-                single.append(xy_data[i + j])
-            training_data.append(single)
-            labels.append(xy_data[i + frames])
-    pbar.close()
-    return [np.array(training_data), np.array(labels)]
-
-
 def make_gif(images, name):
     imageio.mimsave("{}.gif".format(name), images)
 
@@ -184,23 +68,25 @@ def plot_gif(points, simulation):
 
 def prediction_gif(model, initial_data, gif_length):
     prediction = initial_data
-    plt.scatter(*zip(*prediction[0]))
+    plt.scatter(*zip(*prediction[0, :, 0:2]))
     plt.Figure(figsize=[5, 5], dpi=300)
     plt.xlim([-1, 1])
     plt.ylim([-1, 1])
     plt.show()
     plt.clf()
-    prediction = np.reshape(prediction, newshape=(1, 1, np.shape(prediction)[1], 2))
+    prediction = np.reshape(prediction, newshape=(1, 1, np.shape(prediction)[1], 4))
     image_array = []
     for i in range(gif_length):
         fig = plt.Figure(figsize=[5, 5], dpi=300)
         canvas = FigureCanvas(fig)
         ax = fig.gca()
-        prediction = model(prediction)
-        ax.scatter(*zip(*prediction[0]))
+        n_prediction = model(prediction)
+        prediction[0,:,:,2:] = n_prediction.numpy()
+        prediction[0, :, :, :2] = prediction[0, :, :, :2] + (n_prediction.numpy()/100)
+        ax.scatter(*zip(*prediction[0, 0, :, 0:2]))
 
         # ax.scatter(prediction[0])
-        prediction = np.reshape(prediction, newshape=(1, 1, np.shape(prediction)[1], 2))
+        # prediction = np.reshape(prediction, newshape=(1, 1, np.shape(prediction)[1], 2))
         ax.axvline(0)
         ax.set_xlim([-1, 1])
         ax.set_ylim([-1, 1])
@@ -211,40 +97,77 @@ def prediction_gif(model, initial_data, gif_length):
         image_array.append(image)
     make_gif(image_array, "pred_gif")
 
-def velocity_calculation(data):
-    velocity = []
-    for i in range(len(data)-1):
-        velocity.append(data[i]-data[i+1])
+def velocity_calculation(data, scaling, time_step):
+    data = np.array(data)
+    velocity = [data[time_step] - data[0]]
+    for i in range(1, len(data)-time_step):
+        velocity.append((data[i+time_step]-data[i])*scaling)
+    return np.array(velocity)
+
+
+def load_data(points, frames, time_step):
+    training_data = []
+    labels = []
+    pbar = tqdm(total=2)
+    for simulation in range(2):
+        pbar.update()
+        data_names = glob.glob("training_data/Simulation_{}_points_{}/*".format(simulation, points))
+        folder_length = len(data_names)
+        data_array = []
+        for file_num in range(3, folder_length + 1):
+            data = np.load("training_data/Simulation_{}_points_{}/data_{}.npy".format(simulation, points, file_num))
+            data_array.append(data)
+        xy_data = []
+        for data in data_array:
+            xy_array = []
+            for i in range(points):
+                xy = []
+                for j in range(2):
+                    xy.append(data[j][i])
+                xy_array.append(xy)
+            xy_data.append(xy_array)
+        vel_data = velocity_calculation(xy_data, 100, time_step)
+        for i in range(0, len(xy_data) - frames*time_step - time_step):
+            single = []
+            for j in range(0, frames):
+                row = np.array(xy_data[i + j*time_step + time_step])
+                row = np.append(row, vel_data[i+j*time_step], axis=1)
+                single.append(row)
+                # single.append(vel_data[i + j])
+            training_data.append(single)
+            labels.append(vel_data[i + frames*time_step])
+    pbar.close()
+    return [np.array(training_data), np.array(labels)]
 
 
 def main():
     print("Running Training")
     activation = activations.relu
     optimizer = optimizers.Adam()
-    frames = 1
+    frames = 2
     points = 100
     kernal_size = 32
-    data = load_data(points, frames)
-    training_data = data[0]
-    labels = data[1]
-    labels = labels - training_data[:, -1, :, :]
-    max = np.max(labels, axis=2)
-    max = np.max(labels, axis=1)
-    t1 = training_data[28]
-    l1 = data[1][28]
-    a1 = l1-t1
-    t2 = training_data[29]
-    l2 = data[1][29]
-    a2 = l2 - t2
-    b1 = a1+a2
-    b2 = a1-a2
-    model = models.resnet(activation, optimizer, 100, frames)
-    history = model[0].fit(data[0], data[1], epochs=4, shuffle=True)
+
+    # a1 = np.array([[[1],[1],[1]], [[1],[1],[1]]])
+    # a2 = np.array([[[1], [1], [1]], [[1], [1], [1]]])
+
+    # datas = tf.data.Dataset.from_tensor_slices((a1, a2))
+
+    data = load_data(points, frames, 5)
+    data[0] = np.transpose(data[0], axes=(0, 2, 1, 3))
+    datas = tf.data.Dataset.from_tensor_slices((data[0], data[1])).batch(32)
+
+    # model = models.resnet(activation, optimizer, 100, frames)[0]
+    model = models.graph_network(frames, activation, 4, 4, 2)
+
+    history = model.fit(datas, epochs=5, shuffle=True)
+    # model = tf.keras.models.load_model("saved_models/10_03_2022_16_25")
+
     today = datetime.today()
-    dt_string = today.strftime("%d_%m_%Y_%H_%M")
-    directory = "saved_models/" + dt_string
-    model[0].save(directory)
-    prediction_gif(model[0], data[0][400], 10)
+    # dt_string = today.strftime("%d_%m_%Y_%H_%M")
+    # directory = "saved_models/" + dt_string
+    # model.save(directory)
+    # prediction_gif(model, data[0][400], 10)
 
 
 if __name__ == "__main__":